74050c5accc63a60d63b9c3b4c05e511ecb6a60a
Luiz Aquino <quinnpw@live.com>
2024-04-08 19:09:41 -0300

    add function consumo api

--- src/main/java/br/com/luiz/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/luiz/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package br.com.luiz.screenmatch;
 
+import br.com.luiz.screenmatch.service.ConsumoApi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,11 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Primeiro projeto Spring sem web");
+
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		System.out.println(json);
+
+
 	}
 }
