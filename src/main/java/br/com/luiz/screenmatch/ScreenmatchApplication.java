63a25f56c4113f73cafe7945aa2d453907772bbf
Luiz Aquino <quinnpw@live.com>
2024-05-21 13:14:10 -0300

    adicionado classes de conversão e serialização de dados

--- src/main/java/br/com/luiz/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/luiz/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.luiz.screenmatch;
 
+import br.com.luiz.screenmatch.model.DadosSerie;
 import br.com.luiz.screenmatch.service.ConsumoApi;
+import br.com.luiz.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,6 +20,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		var consumoApi = new ConsumoApi();
 		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
 		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 
 
 	}
