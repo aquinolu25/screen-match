4520e1d2081daded7303a33ec672f2aa01f7a6de
Luiz Aquino <quinnpw@live.com>
2024-06-03 20:09:22 -0300

    implementação mapeamento de episodios, utilizando funções lambda

--- src/main/java/br/com/luiz/screenmatch/principal/Principal.java
+++ src/main/java/br/com/luiz/screenmatch/principal/Principal.java
@@ -1,5 +1,6 @@
 package br.com.luiz.screenmatch.principal;
 
+import br.com.luiz.screenmatch.model.DadosEpisodio;
 import br.com.luiz.screenmatch.model.DadosSerie;
 import br.com.luiz.screenmatch.model.DadosTemporada;
 import br.com.luiz.screenmatch.service.ConsumoApi;
@@ -36,5 +37,14 @@ public class Principal {
 
         }
         temporadas.forEach(System.out::println);
+
+//        for (int i = 0; i<=dados.totalTemporadas(); i++) {
+//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//            for (int j = 0; j<episodiosTemporada.size(); j++) {
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
     }
 }
