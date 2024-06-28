dc09c9eabb068662b0b6d50bbb07b796fb3b7ab8
Luiz Aquino <quinnpw@live.com>
2024-06-28 13:21:17 -0300

    adicionado metodo que mostra série a partir do id

--- src/main/java/br/com/luiz/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/luiz/screenmatch/controller/SerieController.java
@@ -4,6 +4,7 @@ import br.com.luiz.screenmatch.dto.SerieDTO;
 import br.com.luiz.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +31,11 @@ public class SerieController {
     @GetMapping("/lancamentos")
     public List<SerieDTO> obterLancamentos() {
         return service.obterLancamentos();
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO obterPorId(@PathVariable Long id) {
+        return service.obterPorId(id);
     }
 
 }
