af55c979279d3a618e0fbb25a44ccc17028156d5
Luiz Aquino <quinnpw@live.com>
2024-06-28 11:54:36 -0300

    criação package service e classe, para desaclopamento do código, inserindo toda regra de negócio na service

---
+++ src/main/java/br/com/luiz/screenmatch/service/SerieService.java
@@ -0,0 +1,25 @@
+package br.com.luiz.screenmatch.service;
+
+import br.com.luiz.screenmatch.dto.SerieDTO;
+import br.com.luiz.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieService {
+
+    @Autowired
+    private SerieRepository repository;
+
+    public List<SerieDTO> obterTodasAsSeries() {
+        return repository.findAll()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
+
+    }
+
+}
