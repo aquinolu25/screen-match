ecd05c29e8c4a6baa86c245e2b759a9bf3d2fb83
aquinolu25 <quinnpw@live.com>
2025-09-13 23:12:20 -0300

    Changed access modifiers in Filme class and created getter method

--- src/Filme.java
+++ src/Filme.java
@@ -3,9 +3,13 @@ public class Filme {
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double somaDasAvaliacoes;
-    int totalDeAvaliacoes;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
     int duracaoEmMinutos;
+
+    int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
 
     void exibeFichaTecnica() {
         System.out.println("Nome do filme: " + nome);
